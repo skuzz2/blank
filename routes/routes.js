d169462cd52031abe0f5adedde619cc2a16e1355
jlittle92 <jlittle@student.neumont.edu>
2017-05-30 10:21:47 -0600

    Auto stash before merge of "master" and "origin/master"

--- routes/routes.js
+++ routes/routes.js
@@ -14,7 +14,7 @@ var userSchema = mongoose.Schema({
     admin: Boolean,
     email: String,
     age: Number,
-    answers: [3]
+    answers: []
 });
 
 var User = mongoose.model('User_Collection', userSchema);
@@ -32,77 +32,77 @@ var User = mongoose.model('User_Collection', userSchema);
 *
 * */
 
-exports.index = function (req, res) {
-    Person.find(function (err, person) {
-        if (err) return console.error(err);
-        res.render('index', {
-            title: 'People List',
-            people: person
-        });
-    });
-};
+// exports.index = function (req, res) {
+//     Person.find(function (err, person) {
+//         if (err) return console.error(err);
+//         res.render('index', {
+//             title: 'People List',
+//             people: person
+//         });
+//     });
+// };
 
 exports.create = function (req, res) {
     res.render('create', {
-        title: 'Add Person'
+        title: 'Add User'
     });
 };
 
-exports.createPerson = function (req, res) {
-    var person = new Person({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        age: req.body.age,
-        species: req.body.species,
-        victims: req.body.victims
+exports.createUser = function (req, res) {
+    var user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        admin: req.body.admin,
+        email: req.body.email,
+        age: req.body.age
     });
-    person.save(function (err, person) {
+    user.save(function (err, user) {
         if (err) return console.error(err);
-        console.log(req.body.firstname + ' added');
+        console.log(req.body.username + ' added');
     });
     res.redirect('/');
 };
 
-exports.edit = function (req, res) {
-    Person.findById(req.params.id, function (err, person) {
-        if (err) return console.error(err);
-        res.render('edit', {
-            title: 'Edit Person',
-            person: person
-        });
-    });
-};
-
-exports.editPerson = function (req, res) {
-    Person.findById(req.params.id, function (err, person) {
-        if (err) return console.error(err);
-        person.firstname = req.body.firstname;
-        person.lastname = req.body.lastname;
-        person.age = req.body.age;
-        person.species = req.body.species;
-        person.victims = req.body.victims;
-        person.save(function (err, person) {
-            if (err) return console.error(err);
-            console.log(req.body.firstname + ' updated');
-        });
-    });
-    res.redirect('/');
-
-};
-
-exports.delete = function (req, res) {
-    Person.findByIdAndRemove(req.params.id, function (err, person) {
-        if (err) return console.error(err);
-        res.redirect('/');
-    });
-};
-
-exports.details = function (req, res) {
-    Person.findById(req.params.id, function (err, person) {
-        if (err) return console.error(err);
-        res.render('details', {
-            title: person.firstname + "'s Details",
-            person: person
-        });
-    });
-};
+// exports.edit = function (req, res) {
+//     Person.findById(req.params.id, function (err, person) {
+//         if (err) return console.error(err);
+//         res.render('edit', {
+//             title: 'Edit Person',
+//             person: person
+//         });
+//     });
+// };
+//
+// exports.editPerson = function (req, res) {
+//     Person.findById(req.params.id, function (err, person) {
+//         if (err) return console.error(err);
+//         person.firstname = req.body.firstname;
+//         person.lastname = req.body.lastname;
+//         person.age = req.body.age;
+//         person.species = req.body.species;
+//         person.victims = req.body.victims;
+//         person.save(function (err, person) {
+//             if (err) return console.error(err);
+//             console.log(req.body.firstname + ' updated');
+//         });
+//     });
+//     res.redirect('/');
+//
+// };
+//
+// exports.delete = function (req, res) {
+//     Person.findByIdAndRemove(req.params.id, function (err, person) {
+//         if (err) return console.error(err);
+//         res.redirect('/');
+//     });
+// };
+//
+// exports.details = function (req, res) {
+//     Person.findById(req.params.id, function (err, person) {
+//         if (err) return console.error(err);
+//         res.render('details', {
+//             title: person.firstname + "'s Details",
+//             person: person
+//         });
+//     });
+// };
