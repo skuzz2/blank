33406010dfe2b39cc9aca86352bd0532c4630af3
jlittle92 <jlittle@student.neumont.edu>
2017-05-31 09:50:41 -0600

    Add bcrypt and dependencies

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ var express = require('express'),
     pug = require('pug'),
     path = require('path'),
     route = require('./routes/routes.js'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    bcrypt = require('bcrypt-nodejs'),
+    hash;
 
 var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
-
 
 var urlencodedParser = bodyParser.urlencoded({
     extended: true
