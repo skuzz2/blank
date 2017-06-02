95403067e4fdcc20639fc95b058c51e218b725ab
jlittle92 <jlittle@student.neumont.edu>
2017-06-02 09:38:43 -0600

    Auto stash before merge of "master" and "origin/master"

--- public/draw_graph.js
+++ public/draw_graph.js
@@ -1,2 +1,10 @@
 var canvas = document.getElementById('my_canvas');
 var ctx = canvas.getContext('2d');
+
+function drawGraph(data, color){
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    /*
+    * DRAW THE BAR GRAPH
+    * */
+}
