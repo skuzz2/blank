cd02bb88a13944e9077335f2d4e35155b9b8959f
jlittle92 <jlittle@student.neumont.edu>
2017-06-06 17:54:00 -0600

    Auto stash before merge of "master" and "origin/master"

--- public/draw_graph.js
+++ public/draw_graph.js
@@ -1,14 +1,11 @@
-var question_one_data/*mongodata*/;
-var question_two_data;
-var question_three_data;
-console.log(question_one_data);
-
 var canvas_one = document.getElementById('my_canvas1');
 var ctx_one = canvas_one.getContext('2d');
 var canvas_two = document.getElementById('my_canvas2');
 var ctx_two = canvas_two.getContext('2d');
 var canvas_three = document.getElementById('my_canvas3');
 var ctx_three = canvas_three.getContext('2d');
+
+console.log(question_one_data);
 
 function drawGraph(data, color, context){
     context.strokeStyle = color;
